82d7c33df941d11c2cb9e24763ceb29dadd96581
ivo <ivo.bathke@gmail.com>
2013-11-11 22:14:07 +0100

    added tests

---
+++ tests/integration/RedBeanServiceProviderTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace Ivoba\Silex;
+
+use Silex\Application;
+use RedBean_Facade as R;
+
+class RedBeanServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRegister()
+    {
+        $app = new Application();
+
+        $app->register(new RedBeanServiceProvider(), array('db.options' => array(
+            'dsn' => 'sqlite:'.__DIR__.'/test.sqlite'
+        )));
+        $app['db'];//db init
+        $this->assertSame('sqlite:'.__DIR__.'/test.sqlite', $app['db.options']['dsn']);
+
+        //Todo create db, check db, delete db
+    }
+
+    public function testDB()
+    {
+        $app = new Application();
+        $app->register(new RedBeanServiceProvider(array('db.options' => array(
+            'dsn' => 'sqlite:'.__DIR__.'/test.sqlite'
+        ))));
+        $app['db'];//db init
+        $post = R::dispense('post');
+        $post->text = 'Hello World';
+        $id = R::store($post);       //Create or Update
+        $fetchedPost = R::load('post',$id); //Retrieve
+        $this->assertSame($post->text, $fetchedPost->text);
+        $this->assertTrue(file_exists(__DIR__.'/test.sqlite'));
+        unlink(__DIR__.'/test.sqlite');
+    }
+}
